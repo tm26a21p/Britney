39188d6db1e1d5a3e326a22c1445879d1d47a5e6
tm26a21p <louis.piochaud@epitech.eu>
2024-06-25 09:28:13 -0700

    feat: path arg for britney.generate_issue

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>
 {
     let b: Britney = Britney::new();
     b.check().await?;
-    let x = b.generate_issue().await?;
+    let code_path = "src/issue.rs";
+    let x = b.generate_issue(code_path).await?;
     println!("{:?}", x);
     Ok(())
 }
