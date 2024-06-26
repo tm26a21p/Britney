b80b1997c45884904a9a74192f0293f292257229
tm26a21p <louis.piochaud@epitech.eu>
2024-06-25 20:36:13 -0700

    del: print the answer by the main

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>
     let b: Britney = Britney::new();
     b.check().await?;
     let code_path = "src/issue.rs";
-    let x = b.generate_issue(code_path).await?;
-    println!("{:?}", x);
+    let _ = b.generate_issue(code_path).await?;
     Ok(())
 }
