3456d94f663a11d0d1b64e419e93fad6f2e57c0f
tm26a21p <louis.piochaud@epitech.eu>
2024-06-27 18:19:27 -0700

    upd: changing files

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>
 {
     let b: Britney = Britney::new(Some("issue_templates/default.md"));
     b.check().await?;
-    let code_path = "src/github_client.rs";
+    let code_path = "src/main.rs";
     let issue: Issue = b.generate_issue_from_file(code_path).await?;
     b.post_issue(issue).await?;
     Ok(())
